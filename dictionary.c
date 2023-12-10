ddcf49dbfa25074ab9cb8fb7bbe5cc0ee13283fb
HarvyDev <franciscolbantunes@gmail.com>
2023-12-10 16:31:22 +0000

    Dictionary functions done

---
+++ dictionary.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dictionary.h"
+// #include <glib.h>
+
+Dictionary *dictionary_create() {
+	Dictionary *dictionary = g_new(Dictionary, 1);
+	dictionary->wordset = g_hash_table_new(g_str_hash, g_str_equal);
+	return dictionary;
+}
+
+void dictionary_add(Dictionary *dictionary, const char *filename) {
+	FILE *file;
+	char line[32];
+
+	// Open the file 
+	file = fopen(filename, "r");
+
+	if (file == NULL) {
+		perror("Error opening file");
+		return;
+	}
+
+	while (fgets(line, sizeof(line), file) != NULL) {		
+		size_t len = strlen(line);
+		if (len > 0 && line[len - 1] == '\n') {
+			line[len - 1] = '\0';
+		}
+		g_hash_table_insert(dictionary->wordset, g_strdup(line), GINT_TO_POINTER(1));
+	}
+
+	fclose(file);
+}
+
+int dictionary_lookup(Dictionary *dictionary, const char *word){
+	// Implement using GLib data structures
+	return g_hash_table_contains(dictionary->wordset, word);
+}
+
+void dictionary_destroy(Dictionary *dictionary) {
+	g_hash_table_destroy(dictionary->wordset);
+	g_free(dictionary);
+}
+
+// extra functions
+
+void print_all_values(gpointer key, gpointer value, gpointer user_data) {
+    printf("Key: %s, Value: %d\n", (char*)key, GPOINTER_TO_INT(value));
+}
+
+void dictionary_print_all_values(Dictionary *dictionary) {
+    g_hash_table_foreach(dictionary->wordset, print_all_values, NULL);
+}
+
+
+int main() {
+	// Initialize dictionary
+	Dictionary *dict = dictionary_create();
+	// Populate dictionary
+	dictionary_add(dict, "wordlist-ao-20101027.txt");
+	// Check if word exists
+	int exists = dictionary_lookup(dict, "Maria");
+	printf("%d\n", exists);
+	// Release memory
+	dictionary_destroy(dict);
+	return 0;
+}
---
+++ dictionary.h
@@ -0,0 +1,15 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <glib.h>
+
+typedef struct dictionary {
+	GHashTable *wordset;	
+} Dictionary;
+
+Dictionary *dictionary_create();
+void dictionary_add(Dictionary *dictionary, const char *filename);
+int dictionary_lookup(Dictionary *dictionary, const char *word);
+void dictionary_destroy(Dictionary *dictionary); 
+
+#endif
